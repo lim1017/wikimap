34b62d61b78b48addb704d447063a18052f8a6ec
tom <lim1017@hotmail.com>
2020-03-17 00:51:10 -0400

    axcv

--- public/mapCreate.js
+++ public/mapCreate.js
@@ -23,6 +23,7 @@ $("document").ready(function() {
   });
 
   $("#edit").on("click", function() {
+
     editMode = true;
 
   });
@@ -49,6 +50,7 @@ $("document").ready(function() {
 
     } else{
     savePoints();
+    window.alert("Saved");
     editMode = false;
     }
   });
@@ -149,30 +151,6 @@ $("document").ready(function() {
     $(".title-box").focus();
   });
 
-  // Get the modal
-  var modal = document.getElementById("myModal");
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("edit");
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-  modal.style.display = "block";
-  }
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-  modal.style.display = "none";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
 });
 
